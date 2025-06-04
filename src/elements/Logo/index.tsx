17cae0a0dd21b943f6f0982f454cd3d8c3deda18
Rnbsov <rnbsov@gmail.com>
2025-06-04 10:11:22 +0500

    ✨ ui: Update icon bg and radius
    Changelog: ui

--- src/elements/Logo/index.tsx
+++ src/elements/Logo/index.tsx
@@ -10,8 +10,8 @@ export const Logo = () => {
 
 export const Icon = () => {
   return (
-    <span style={{ position: 'relative', width: '50px', height: '50px' }}>
-      <Image src="/favicon-32x32.svg" alt="" fill />
+    <span style={{ position: 'relative', width: '50px', height: '50px', background: 'white', borderRadius: '10px' }}>
+      <Image src="/favicon-32x32.png" alt="" fill />
     </span>
   )
 }
